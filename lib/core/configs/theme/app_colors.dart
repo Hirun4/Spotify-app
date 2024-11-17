c877ffedbc3c2bff16e07454a719c4d00a626ef7
jayathu sanka <shjayathusanka@gmail.com>
2024-11-17 21:05:43 +0530

    theme

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xff42C83C); // Primary color
-  static const Color lightBackground = Color(0xffF2F2F2); // Light background color
-  static const Color darkBackground = Color(0xffF0D0CC); // Dark background color
+  static const Color primary = Color(0xff42C83C); 
+  static const Color lightBackground = Color(0xffF2F2F2); 
+  static const Color darkBackground = Color(0xff0D0C0C);
+  static const grey = Color(0xffBEBEBE);
+  static const darkGrey = Color(0xff343434);
+
 }
