c85a63530eb333a17502b887dba945285aa5ed6e
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-02 16:33:50 +0530

    bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:spotify_app/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -14,9 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         //title: 'Flutter Demo',
-        theme: AppTheme.lightTheme, 
-        home: const SplashPage()
-    );
+        theme: AppTheme.lightTheme,
+        home: const SplashPage());
   }
-} 
+}
--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,4 +1,4 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify_app/core/configs/assets/app_vectors.dart';
 
@@ -8,11 +8,7 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SvgPicture.asset(
-          AppVectors.logo
-        )
-      ),
+      body: Center(child: SvgPicture.asset(AppVectors.logo)),
     );
   }
 }
