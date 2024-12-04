74da75932f0da419e6d4669f3c6bf38e3197be77
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-04 17:50:52 +0530

    thememode.system

--- lib/presentation/choose_mode/bloc/theme_cubit.dart
+++ lib/presentation/choose_mode/bloc/theme_cubit.dart
@@ -4,7 +4,7 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 class ThemeCubit extends HydratedCubit<ThemeMode>{
 
-  ThemeCubit() : super(ThemeMode)
+  ThemeCubit() : super(ThemeMode.system)
   @override
   ThemeMode? fromJson(Map<String, dynamic> json) {
     // TODO: implement fromJson
