740805dc03569c03ad5e7f149927286898f549f3
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-04 15:10:39 +0530

    choose mode page

--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../common/widgets/button/basic_app_button.dart';
+import '../../../core/configs/assets/app_images.dart';
+import '../../../core/configs/assets/app_vectors.dart';
+import '../../../core/configs/theme/app_colors.dart';
 
 class chooseModePage extends StatelessWidget {
   const chooseModePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AppImages.introBG,
+                    ))),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                const Spacer(),
+                const Text(
+                  'Listen to Your Favorite Tunes Anytime, Anywhere',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18),
+                ),
+                const SizedBox(
+                  height: 21,
+                ),
+                const Text(
+                  "Discover millions of songs, albums, and playlists across all genres. Whether you're relaxing, working out, or partying, we've got the perfect soundtrack for every moment.",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                      fontSize: 13),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                const chooseModePage()),
+                      );
+                    },
+                    title: 'Get Started')
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
