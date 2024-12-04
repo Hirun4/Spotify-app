abd857c2c5f097efbb1ca37685a46ec43a380be4
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-04 14:28:39 +0530

    choose mode page

---
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class chooseModePage extends StatelessWidget {
+  const chooseModePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    )
+  }
+}
