7b25bc52395bac052437347013abb7e48f935b3e
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-13 14:41:48 +0530

    changes

--- lib/data/models/auth/create_user_req.dart
+++ lib/data/models/auth/create_user_req.dart
@@ -1,6 +1,8 @@
-class SigninUserReq {
+class CreateUserReq {
+  final String fullName;
   final String email;
   final String password;
 
-  SigninUserReq({required this.email, required this.password});
+  CreateUserReq(
+      {required this.fullName, required this.email, required this.password});
 }
