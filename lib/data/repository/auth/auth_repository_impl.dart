df011e6db23455d8ec2825c2f7fd5d769fccbea6
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-13 16:12:55 +0530

    sign in

--- lib/data/repository/auth/auth_repository_impl.dart
+++ lib/data/repository/auth/auth_repository_impl.dart
@@ -1,13 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:spotify_app/data/models/auth/create_user_req.dart';
+import 'package:spotify_app/data/models/auth/signin_user_req.dart';
 import 'package:spotify_app/data/sources/auth/auth_firebase_service.dart';
 import 'package:spotify_app/domain/repository/auth/auth.dart';
 import 'package:spotify_app/service_locator.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
   @override
-  Future<void> signin() {
-    return await sl<AuthFirebaseService>().signin(Sign);
+  Future<Either> signin(SigninUserReq signinUserReq) async {
+    return await sl<AuthFirebaseService>().signin(signinUserReq);
   }
 
   @override
