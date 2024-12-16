48ec25728281dd606ca8e59aa60bc6c83a23032f
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-16 15:29:03 +0530

    hideback

--- lib/common/widgets/appbar/app_bar.dart
+++ lib/common/widgets/appbar/app_bar.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:spotify_app/common/helpers/is_dark_mode.dart';
 
 class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
-  final Widget ? title;
+  final Widget? title;
+  final bool hideBack;
   const BasicAppbar({
     this.title,
-    super.key, 
+    this.hideBack = false,
+    super.key,
     //required SvgPicture title
-    });
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +20,17 @@ class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
       centerTitle: true,
       title: title ?? const Text(''),
       leading: IconButton(
-        onPressed: (){
+        onPressed: () {
           Navigator.pop(context);
         },
         icon: Container(
           height: 50,
           width: 50,
           decoration: BoxDecoration(
-            color: context.isDarkMode ? Colors.white.withOpacity(0.03) : Colors.black.withOpacity(0.04),
-            shape: BoxShape.circle
-          ),
+              color: context.isDarkMode
+                  ? Colors.white.withOpacity(0.03)
+                  : Colors.black.withOpacity(0.04),
+              shape: BoxShape.circle),
           child: Icon(
             Icons.arrow_back_ios_new,
             size: 15,
@@ -37,7 +40,7 @@ class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
       ),
     );
   }
-  
+
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
