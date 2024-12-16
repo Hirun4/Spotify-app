97fe921bc364709a2f133890108815e8dcaf62d8
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-16 21:19:49 +0530

    setters set

--- lib/data/models/song/song.dart
+++ lib/data/models/song/song.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SongModel {
-  final String title;
-  final String artist;
-  final num duration;
-  final Timestamp releaseDate;
+  String title;
+  String artist;
+  num duration;
+  Timestamp releaseDate;
 
   SongModel({
     required this.title,
@@ -13,5 +13,10 @@ class SongModel {
     required this.releaseDate,
   });
 
-  SongModel.fromJson(Map<String, dynamic> data) {}
+  SongModel.fromJson(Map<String, dynamic> data) {
+    title = data['title'];
+    artist = data['artist'];
+    duration = data['duration'];
+    releaseDate = data['releaseDate'];
+  }
 }
