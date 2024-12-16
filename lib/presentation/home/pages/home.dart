d6913196b9ce42dbdaba1d7a8c5fccc910b95b62
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-16 15:19:55 +0530

    app bar

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../common/widgets/appbar/app_bar.dart';
+import '../../../core/configs/assets/app_vectors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: BasicAppbar(
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          height: 40,
+          width: 40,
+        ),
+      ),
+    );
   }
 }
