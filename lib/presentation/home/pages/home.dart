af91f1879d7fd5eb40c35cdc2dd5be78a1554e49
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-16 18:29:54 +0530

    tab

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -5,8 +5,22 @@ import 'package:spotify_app/core/configs/assets/app_images.dart';
 import '../../../common/widgets/appbar/app_bar.dart';
 import '../../../core/configs/assets/app_vectors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+  }
 
   @override
   Widget build(BuildContext context) {
