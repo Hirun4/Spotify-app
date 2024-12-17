08095af969aa64270e118b6655a55c18dae60ca0
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-17 18:00:01 +0530

    song loaded

--- lib/presentation/home/bloc/news_songs_state.dart
+++ lib/presentation/home/bloc/news_songs_state.dart
@@ -1,5 +1,10 @@
+import 'package:spotify_app/domain/entities/song/song.dart';
+
 abstract class NewsSongsState {}
 
 class NewsSongsLoading extends NewsSongsState {}
 
-class NewsSongsLoaded extends NewsSongsState {}
+class NewsSongsLoaded extends NewsSongsState {
+  final List<SongEntity> songs;
+  NewsSongsLoaded({required this.songs});
+}
