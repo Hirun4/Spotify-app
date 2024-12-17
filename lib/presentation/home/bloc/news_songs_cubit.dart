e8c07432015ad8bbeca7f7a860aa696f2b79d2b9
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-17 18:15:38 +0530

    cubit

---
+++ lib/presentation/home/bloc/news_songs_cubit.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify_app/domain/usecases/song/get_news_songs.dart';
+import 'package:spotify_app/presentation/home/bloc/news_songs_state.dart';
+
+import '../../../service_locator.dart';
+
+class NewsSongsCubit extends Cubit<NewsSongsState> {
+  NewsSongsCubit(super.initialState);
+
+  Future<void> getNewsSongs() async {
+    var returnedSongs = await sl<GetNewsSongsUsecase>().call();
+
+    returnedSongs.fold((l) {}, (r) {});
+  }
+}
