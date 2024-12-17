cf9aa89a6a93248e7ab5f995698a20e2f5eb123b
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-17 18:55:27 +0530

    sized box

--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify_app/presentation/home/bloc/news_songs_cubit.dart';
 
 class NewsSongs extends StatelessWidget {
   const NewsSongs({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (_) => NewsSongsCubit(),
+      child: SizedBox(
+        height: 200,
+        child: ListView.separated(
+            itemBuilder: itemBuilder,
+            separatorBuilder: separatorBuilder,
+            itemCount: itemCount),
+      ),
+    );
   }
 }
