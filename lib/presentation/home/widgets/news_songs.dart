adf91f6e02c930677d68c12af713514ee6775804
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-17 18:51:50 +0530

    news songs

---
+++ lib/presentation/home/widgets/news_songs.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class NewsSongs extends StatelessWidget {
+  const NewsSongs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
