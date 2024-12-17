ebceaa71f677f93c44bbb80d97edf3fd82e90aed
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-17 11:41:02 +0530

    getnews song impl

--- lib/data/repository/song/song_repository_impl.dart
+++ lib/data/repository/song/song_repository_impl.dart
@@ -1,3 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:spotify_app/data/sources/song/song.dart';
 import 'package:spotify_app/domain/repository/song/song.dart';
 
-class SongRepositoryImpl extends SongsRepository {}
+import '../../../service_locator.dart';
+
+class SongRepositoryImpl extends SongsRepository {
+  @override
+  Future<Either> getNewsSongs() async {
+    return await sl<SongFirebaseService>().getNewsSongs();
+  }
+}
