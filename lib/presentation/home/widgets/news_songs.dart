ed6b1c7cabf1239b88d78d373b1e7441bdf9ef19
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-17 19:21:36 +0530

    newssongs

--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotify_app/presentation/home/bloc/news_songs_cubit.dart';
 
+import '../bloc/news_songs_state.dart';
+
 class NewsSongs extends StatelessWidget {
   const NewsSongs({super.key});
 
@@ -11,12 +13,25 @@ class NewsSongs extends StatelessWidget {
     return BlocProvider(
       create: (_) => NewsSongsCubit(),
       child: SizedBox(
-        height: 200,
-        child: ListView.separated(
-            itemBuilder: itemBuilder,
-            separatorBuilder: separatorBuilder,
-            itemCount: itemCount),
-      ),
+          height: 200,
+          child: BlocBuilder<NewsSongsCubit, NewsSongsState>(
+            builder: (context, State) {
+              if (State is NewsSongsLoading) {
+                return CircularProgressIndicator();
+              }
+              if (State is NewsSongsLoaded) {
+                return _songs();
+              }
+              return Container();
+            },
+          )),
     );
+  }
+
+  Widget _songs() {
+    return ListView.separated(
+        itemBuilder: itemBuilder,
+        separatorBuilder: separatorBuilder,
+        itemCount: itemCount);
   }
 }
