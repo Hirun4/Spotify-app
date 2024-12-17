e1b8b5035cc5aac0d7c485c07a3c12be1d866884
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-17 07:49:40 +0530

    firebase impl

--- lib/data/sources/song/song.dart
+++ lib/data/sources/song/song.dart
@@ -3,3 +3,11 @@ import 'package:dartz/dartz.dart';
 abstract class SongFirebaseService {
   Future<Either> getNewsSongs();
 }
+
+class SongFirebaseImpl extends SongFirebaseService {
+  @override
+  Future<Either> getNewsSongs() {
+    // TODO: implement getNewsSongs
+    throw UnimplementedError();
+  }
+}
