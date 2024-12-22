b54ac062cbedab3120a40313f00915f4473955c6
jayathu sanka <shjayathusanka@gmail.com>
2024-12-22 16:15:25 +0530

    4.40

---
+++ lib/core/configs/constants/app_urls.dart
@@ -0,0 +1,6 @@
+class AppURLs {
+
+  static const firestorage = 'https://firebasestorage.googleapis.com/v0/b/gaming-94110.appspot.com/o/covers%2F';
+  static const mediaAlt = 'alt=media';
+
+}
---
+++ lib/domain/usecases/song/get_play_list.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:spotify_app/core/usecase/usecase.dart';
+
+import 'package:spotify_app/data/repository/song/song_repository_impl.dart';
+
+import 'package:spotify_app/service_locator.dart';
+
+class GetPlayListUseCase implements UseCase<Either, dynamic> {
+
+  @override
+  Future<Either> call({params}) async {
+    return await sl<SongRepositoryImpl>().getPlayList();
+  }
+}
