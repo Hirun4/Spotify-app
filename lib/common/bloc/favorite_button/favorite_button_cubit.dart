37e172e0a98f54fb86ebd199a83fdcade902d2f7
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-27 08:19:48 +0530

    cubit

---
+++ lib/common/bloc/favorite_button/favorite_button_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify_app/common/bloc/favorite_button/favorite_button_state.dart';
+import 'package:spotify_app/domain/usecases/song/add_or_remove_favorite_song.dart';
+
+import '../../../service_locator.dart';
+
+class FavoriteButtonCubit extends Cubit<FavoriteButtonState> {
+  FavoriteButtonCubit() : super(FavoriteButtonInitial());
+
+  void favoriteButtonUpdated(String songId) async {
+    var result =
+        await sl<AddOrRemoveFavoriteSongUseCase>().call(params: songId);
+
+    result.fold((l) {}, (r) {
+      emit(
+        FavoriteButtonUpdated(),
+      );
+    });
+  }
+}
