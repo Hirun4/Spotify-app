3bd320385b3641a527706a2966eef0a63496504e
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-27 09:15:46 +0530

    favorite button

--- lib/common/widgets/favorite_button/favorite_button.dart
+++ lib/common/widgets/favorite_button/favorite_button.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify_app/common/bloc/favorite_button/favorite_button_cubit.dart';
 
+class FavoriteButton extends StatelessWidget {
+  const FavoriteButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => FavoriteButtonCubit(),
+    );
+  }
+}
